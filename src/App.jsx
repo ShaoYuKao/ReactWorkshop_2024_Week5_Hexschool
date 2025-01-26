109178ecddcc81fd6dcd4e011f3fe2cc2c0aa728
ShaoYu <shaoyu.kao@gmail.com>
2025-01-26 12:11:22 +0800

    feat: 新增產品詳情模態框以顯示選中產品資訊

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import * as bootstrap from "bootstrap";
 import './App.css';
 import FullPageLoading from './components/FullPageLoading';
 
@@ -13,6 +14,7 @@ function App() {
   const [searchTerm, setSearchTerm] = useState(''); // 搜尋關鍵字
   const [loading, setLoading] = useState(false); // 加載狀態
   const searchCategoryRef = useRef(null); // 分類搜尋的參考
+  const [selectedProduct, setSelectedProduct] = useState(null); // 選中的產品
 
   useEffect(() => {
     setLoading(true); // 開始加載
@@ -50,13 +52,134 @@ function App() {
     setSearchTerm(searchCategoryRef.current.value); // 設定搜尋關鍵字
   };
 
+  /**
+   * 處理查看更多的函式。
+   * @param {string} productId - 產品ID。
+   * @returns {void}
+   */
+  const handleViewMore = (productId) => {
+    setLoading(true); // 開始加載
+    axios.get(`${API_BASE}/api/${API_PATH}/product/${productId}`)
+      .then(response => {
+        setSelectedProduct(response.data.product);
+        const productModal = new bootstrap.Modal(document.getElementById('productModal'));
+        productModal.show();
+      })
+      .catch(error => {
+        console.error('Error fetching product details:', error);
+        alert('取得產品細節失敗!!');
+      })
+      .finally(() => {
+        setLoading(false); // 結束加載
+      });
+  };
+
   return (
     <div id="app">
-      {loading && <FullPageLoading />} {/* 加載時顯示 FullPageLoading */}
+      {loading && <FullPageLoading />}
       <div className="container">
         <div className="mt-4">
           {/* 產品Modal */}
+          <div
+            id="productModal"
+            className="modal fade"
+            tabIndex="-1"
+            aria-labelledby="productModalLabel"
+            aria-hidden="true"
+            >
+            <div className="modal-dialog modal-xl">
+              <div className="modal-content border-0">
+                <div className="modal-header bg-dark text-white">
+                  <h5 id="productModalLabel" className="modal-title">
+                    <span>商品資訊</span>
+                  </h5>
+                  <button
+                    type="button"
+                    className="btn-close btn-close-white"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                    ></button>
+                </div>
+                <div className="modal-body">
+                  <div className="row">
+                    <div className="col-sm-4">
+                      <div className="mb-2">
+                        <div className="mb-3">
+                          <label htmlFor="imageUrl" className="form-label">
+                            圖片
+                          </label>
+                          <img className="img-fluid" src={selectedProduct?.imageUrl} alt={selectedProduct?.title} />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-sm-8">
+                      <div className="mb-3">
+                        <label htmlFor="title" className="form-label">標題</label>
+                        <input id="selectedProductTitle" type="text" className="form-control" placeholder="" defaultValue={selectedProduct?.title}
+                disabled />
+                      </div>
 
+                      <div className="row">
+                        <div className="mb-3 col-md-6">
+                          <label htmlFor="category" className="form-label">分類</label>
+                          <input id="category" type="text" className="form-control" placeholder=""
+                  defaultValue={selectedProduct?.category} disabled />
+                        </div>
+                        <div className="mb-3 col-md-6">
+                          <label htmlFor="unit" className="form-label">單位</label>
+                          <input id="unit" type="text" className="form-control" placeholder="" defaultValue={selectedProduct?.unit}
+                  disabled />
+                        </div>
+                      </div>
+
+                      <div className="row">
+                        <div className="mb-3 col-md-6">
+                          <label htmlFor="origin_price" className="form-label">原價</label>
+                          <input id="origin_price" type="number" min="0" className="form-control" placeholder=""
+                  defaultValue={selectedProduct?.origin_price} disabled />
+                        </div>
+                        <div className="mb-3 col-md-6">
+                          <label htmlFor="price" className="form-label">售價</label>
+                          <input id="price" type="number" min="0" className="form-control" placeholder=""
+                  defaultValue={selectedProduct?.price} disabled />
+                        </div>
+                      </div>
+                      <hr />
+
+                      <div className="mb-3">
+                        <label htmlFor="description" className="form-label">產品描述</label>
+                        <textarea id="description" className="form-control" placeholder=""
+                defaultValue={selectedProduct?.description} disabled></textarea>
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="content" className="form-label">說明內容</label>
+                        <textarea id="content" className="form-control" placeholder="" defaultValue={selectedProduct?.content}
+                disabled></textarea>
+                      </div>
+                      <div className="mb-3">
+                        <div className="form-check">
+                        <input id="is_enabled" className="form-check-input" type="checkbox"
+                  checked={selectedProduct?.is_enabled} disabled />
+                          <label className="form-check-label" htmlFor="is_enabled">
+                            是否啟用
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-outline-secondary"
+                    data-bs-dismiss="modal"
+                    >
+                    關閉
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           {/* 產品Modal */}
           <div className="row g-3 align-items-center">
             <div className="col-auto">
@@ -95,7 +218,7 @@ function App() {
                   </td>
                   <td>
                     <div className="btn-group btn-group-sm">
-                      <button type="button" className="btn btn-outline-secondary">
+                      <button type="button" className="btn btn-outline-secondary" onClick={() => handleViewMore(product.id)}>
                         <i className="fas fa-spinner fa-pulse"></i>
                         查看更多
                       </button>
