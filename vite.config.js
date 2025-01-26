97dbd0badf8f905e1f60c39bc974addc6a3c8012
ShaoYu <shaoyu.kao@gmail.com>
2025-01-26 23:20:43 +0800

    build: 更新 Vite 配置以指定基礎路徑並新增部署指令

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ReactWorkshop_2024_Week5_Hexschool/', // 替換為您的 repository 名稱
 })
